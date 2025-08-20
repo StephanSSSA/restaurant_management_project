a6d29512a14f65cd85efc6c5c17137b929424792
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-08-20 08:08:44 -0700

    34165

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'home/index.html')
+    restarunt_name = settings.RESTARUNT_NAME
+    return render(request, 'home/index.html',{"restarunt_name": restarunt_name})
 
 
