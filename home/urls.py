fefd43f357fd37f30bb011108c44bafbcf1073de
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-08-21 06:42:31 -0700

    34166

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
-fromdjango.contrib impot admin
+from django.contrib impot admin
 from django.urls import path,include
 from .views import *
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include('home.urls')),
+    path("about/", views.about, name="about"),
 ]
