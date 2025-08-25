112c724b959be084be189922cbdb781e8a1bcbcf
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-08-24 21:16:45 -0700

    38635

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('',include('home.urls')),
     path("about/", views.about, name="about"),
     path("products/", include(products.urls)),
+    path("contact/", views.contact, name="contact"),
 ]
