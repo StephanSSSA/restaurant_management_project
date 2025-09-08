2d776612ff722f1a49296182818531123134c8e2
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-09-07 20:13:59 -0700

    42554

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import FeedbackForm
+
+def feedback_view(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback')
+    else:
+        form = FeedbackForm()
+    return render(request, "feedback.html", {"form": form})
