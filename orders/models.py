1efc0cbc51bf909814dc13065d816577b48bd80e
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-09-23 19:32:52 -0700

    47892

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50; unique=True)
+
+    def__str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    status = models.Foreignkey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def__str__(self):
+        return f"Order {self.id} - {self.customer_name}"
