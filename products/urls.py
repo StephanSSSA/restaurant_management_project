63bb883f675d1e190bb7d87c0088295d6f9742ea
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-09-26 22:08:25 -0700

    51501

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path("menu/", views.menu, name="menu")
+    path('register/' RegisterUserView.as_view(), name='register-user'),
 ]
