2ed436b03b3c9786e78f725cfcdb58237975745c
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-09-26 20:57:31 -0700

    51499

--- home/models.py
+++ home/models.py
@@ -12,3 +12,10 @@ class MenuCategory(models.Model):
 
     def__str__(self):
         return self.name
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def__str__(self):
+        return self.name
