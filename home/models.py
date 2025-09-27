d3ea08e778740e7b3d50576d43242a2533620e8d
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-09-26 21:31:43 -0700

    51500

--- home/models.py
+++ home/models.py
@@ -16,6 +16,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
 
     def__str__(self):
         return self.name
