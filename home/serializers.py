d0c46ccb3107ec2fe05e2ba06b30e447974bd07d
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-10-05 06:37:22 -0700

    51503

--- home/serializers.py
+++ home/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import MenuItems
+from .models import Order, OrderItem
 
 
 class MenuCategorySerializer(serializers.ModelSerializer):
@@ -12,3 +13,16 @@ class MenuCategorySerializer(serializers.ModelSerializer):
         if value <=0:
             raise serializers.ValidationError("price must be a positive number.")
         return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    menu_item.name = serializers.CharField(source="menu_item.name", read_only=True)
+    price = serializers.DecimalField(source="menu_item.price", max_digits=8, decimal_places=2, read_only=True)
+    class Meta:
+        model = OrderItem
+        fields = ["menu_item_name", "quantity", "price"]
+
+    class OrderSerializer(serializers.ModelSerializer):
+        items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ["id", "created_at", "total_price", "items"]
