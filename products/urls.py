9f86f8160f9da9ed7bc14cfa3a93e8805d458dc3
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-10-14 08:14:51 -0700

    55042

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from .views import CouponValidationView
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path("menu/", views.menu, name="menu")
     path('register/' RegisterUserView.as_view(), name='register-user'),
+    path('coupons/validate/', CouponValidationView.as_view(), name='coupon-validate'),
 ]
