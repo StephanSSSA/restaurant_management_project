f16a8f920c8f5426adf4995482a22f39c17371a3
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-10-14 07:37:25 -0700

    55039

--- orders/utils.py
+++ orders/utils.py
@@ -1,7 +1,7 @@
 import string
 import secrets
 from .models import Coupon 
-from datetime import date
+from datetime import date, time
 from django.db.models import Sum
 from .models import Order
 
@@ -21,3 +21,26 @@ def get_daily_sales_total(target_date: date):
     total = orders.aggregate(total_sum=Sum('toal_price'))['total_sum']
 
     return total or 0
+
+def is_restaurant_open():
+    now = datetime.now()
+    current_day = now.weekday()
+    current_time = now.time()
+
+    opening_hours = {
+        0: (time(9, 0), time(22, 0)),
+        1: (time(9, 0), time(22, 0)),
+        2: (time(9, 0), time(22, 0)),
+        3: (time(9, 0), time(22, 0)),
+        4: (time(9, 0), time(22, 0)),
+        5: (time(9, 0), time(22, 0)),
+        6: (time(9, 0), time(22, 0)),
+    }
+
+    open_time, close_time = opening_hours.get(current_day)
+
+    if open_time, close_time = opening_hours.get(current_day)
+
+    if open_time <= current_time <= close_time:
+        return True
+    return False
