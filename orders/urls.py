7286ccc17cbeb690559483909693dbf499c92ffb
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-10-18 08:00:30 -0700

    55240

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MenuItemViewset
+from .views import OrderDetailView
 
 router = DefaultRouter()
 router.register(r'menu-items', MenuItemViewset, basename='menuitem')
@@ -8,4 +9,5 @@ router.register(r'menu-items', MenuItemViewset, basename='menuitem')
 urlpatterns = [
     path("menu/", views.menu, name="menu"),
     path('', include(router.urls)),
+    path('<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
