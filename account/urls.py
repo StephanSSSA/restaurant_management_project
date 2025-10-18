c313f466b4c9e90b052fb42dae48a1054e21d086
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-10-18 07:11:51 -0700

    55238

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import *
+from .views import UserprofileViewset
+
+user_profile = UserprofileViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+})
 
 urlpatterns = [
-    
+    path('profile/', user_profile, name='user-profile'),
 ]
