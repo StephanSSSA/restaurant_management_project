c926bf4ec4dac5331a4d9f9bef5c1cde7301d950
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-11-03 19:53:59 -0800

    57001

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Restaurant
+from .models import MenuItem, NutritionalInformation
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'operating_days')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_featured')
+
+@admin.register(NutritionalInformation)
+class NutritionalInformationAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'calories', 'protein_grams', 'fat_grams', 'carbohydrate_grams')
