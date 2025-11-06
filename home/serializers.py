d63315c7b4dec73f6bf179fb7482f7fc32483945
stephan j <130647961+StephanSSSA@users.noreply.github.com>
2025-11-05 20:04:25 -0800

    58414

--- home/serializers.py
+++ home/serializers.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers
 from .models import MenuItems
-from .models import Order, OrderItem
-from .models import Table
-from django.contrib.auth.models import user
-from .models import Order
-from .models import ContactFormSubmission
+from .models import MenuItem, Ingredient
 
 
 class MenuCategorySerializer(serializers.ModelSerializer):
@@ -18,51 +14,14 @@ class MenuCategorySerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("price must be a positive number.")
         return value
 
-class OrderItemSerializer(serializers.ModelSerializer):
-    menu_item.name = serializers.CharField(source="menu_item.name", read_only=True)
-    price = serializers.DecimalField(source="menu_item.price", max_digits=8, decimal_places=2, read_only=True)
+class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrderItem
-        fields = ["menu_item_name", "quantity", "price"]
+        model = Ingredient
+        fields = ['id', 'name', 'is_vegan']
 
-    class OrderSerializer(serializers.ModelSerializer):
-        items = OrderItemSerializer(many=True, read_only=True)
+class MenuItemIngredientsSerializer(serializers.Modelserializer):
+    ingredients = IngredientSerializer(many=True)
+
     class Meta:
-        model = Order
-        fields = ["id", "created_at", "total_price", "items"]
-    
-    class TableSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Table
-            fields = ['table_number', 'capacity', 'is_available']
-    
-    class Userprofileserializer(serializers.ModelSerializer):
-        class Meta:
-            model = User
-            fields = ['first_name', 'last_name', 'email']
-            read_only_fields = ['username']
-    
-    class OrderItemSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = OrderItem
-            fields = ['product', 'quantity']
-    class OrderItemSerializer(serializers.ModelSerializer):
-        items = OrderItemSerializer(many=True, read_only=True)
-        class Meta:
-            model = Order
-            fields = ['id', 'customer', 'total_price', 'created_at', 'items']
-    
-    class OrderSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Order
-            fields = '__all__'
-
-    class ContactFormSubmissionSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = ContactFormSubmission
-            fields = ['id', 'name', 'email', 'message', 'submitted_at']
-
-    class MenuItemSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = MenuItem
-            fields = ['id', 'name', 'description', 'price', 'is_featured']
+        model = MenuItem
+        fields = ['id', 'name', 'ingredients']
